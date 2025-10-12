6be6114888f46bdcbfff40cb6ac6c1238bddab86
YeMyatSoe <111271599+YeMyatSoe@users.noreply.github.com>
2025-10-12 15:08:48 +0630

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // <--- CHANGE IS HERE
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/Portfolio">
+  // <BrowserRouter basename="/Portfolio"> <--- REMOVE BROWSERROUTER
+  <HashRouter> // <--- USE HASHROUTER
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
